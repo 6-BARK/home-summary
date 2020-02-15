1c85d25bcccbcfbd7e053371410fca88a53bc2b3
KN <kylenguyen@Kyles-MacBook-Pro-2.local>
2020-02-15 14:54:22 -0800

    README updated and schemas created

---
+++ database/mongoSchema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = new mongoose.Schema;
+
+const houseSchema = new Schema({
+  houseId: Number,          // listing id number
+  price: Number,            // price in $
+  bedCount: Number,         // #bedroom count
+  bathCount: Number,        // #bathroom count
+  sqft: Number,             // size of house in sqft
+  address: String,          // address
+  listingType: String,      // (one of 'for sale', 'for rent', 'sold')
+  zestimate : Number,       // price in $
+  estPayment: Number,       // price in $
+  primaryAgent: Number      // agent id number
+});
+
+const agentSchema = new Schema({
+  agentId: Number,      // agent id number
+  agentName: String,    // agent name
+  agentType: String,    // type of agent, (Premier or Seller's)
+  starCount: Number,    // rating based on reviews, 1-5 stars
+  reviewCount: Number,  // number of reviews agent has
+  phoneNumber: String   // phone number of agent
+})
+
+const contactSchema = new Schema({
+  requestId: Number,        // request id for specific inquiry
+  houseId: Number,          // house id of home
+  agentId: Number,          // agent id for contact
+  customerName: String,     // customer's name
+  customerPhone: String,    // customer's phone number
+  customerMessage: String   // customer's message in request
+})
+
+const House = mongoose.model('houses', houseSchema);
+const Agent = mongoose.model('agents', agentSchema);
+const Contact = mongoose.model('contactRequests', contactSchema);
--- database/psqlSchema.js
+++ database/psqlSchema.js
@@ -1,55 +1,57 @@
 
-
-CREATE TABLE "Agents"
-(
- "agentId"     integer NOT NULL,
- "agentName"   varchar(50) NOT NULL,
- "agentType"   varchar(50) NOT NULL,
- "starCount"   smallint NOT NULL,
- "reviewCount" smallint NOT NULL,
- "phoneNumber" varchar(15) NOT NULL,
- CONSTRAINT "PK_Agent List" PRIMARY KEY ( "agentId" )
+CREATE TABLE "HouseData" (
+	"houseId" serial NOT NULL,
+	"price" integer NOT NULL,
+	"bedCount" smallint NOT NULL,
+	"bathCount" smallint NOT NULL,
+	"address" varchar(50) NOT NULL,
+	"sqft" integer NOT NULL,
+	"listingType" varchar(50) NOT NULL,
+	"zestimate" integer NOT NULL,
+	"estPayment" integer NOT NULL,
+	"primaryAgent" integer NOT NULL,
+	CONSTRAINT "HouseData_pk" PRIMARY KEY ("houseId")
+) WITH (
+  OIDS=FALSE
 );
 
-CREATE TABLE "Contact Agent"
-(
- "requestId"       integer NOT NULL,
- "customerName"    varchar(50) NOT NULL,
- "customerPhone"   varchar(15) NOT NULL,
- "customerMessage" varchar(100) NOT NULL,
- "agentId"         integer NOT NULL,
- CONSTRAINT "PK_Contact Agent" PRIMARY KEY ( "requestId" ),
- CONSTRAINT "FK_72" FOREIGN KEY ( "agentId" ) REFERENCES "Agents" ( "agentId" )
+CREATE TABLE "Agents" (
+	"agentId" serial NOT NULL,
+	"agentName" varchar(50) NOT NULL,
+	"agentType" varchar(50) NOT NULL,
+	"starCount" smallint NOT NULL,
+	"reviewCount" smallint NOT NULL,
+	"phoneNumber" varchar(15) NOT NULL,
+	CONSTRAINT "Agents_pk" PRIMARY KEY ("agentId")
+) WITH (
+  OIDS=FALSE
 );
 
-CREATE INDEX "fkIdx_72" ON "Contact Agent"
-(
- "agentId"
+CREATE TABLE "ContactAgent" (
+	"requestId" serial NOT NULL,
+	"agentId" integer NOT NULL,
+	"houseId" integer NOT NULL,
+	"customerName" varchar(50) NOT NULL,
+	"customerPhone" varchar(15) NOT NULL,
+	"customerMessage" varchar(250) NOT NULL,
+	CONSTRAINT "ContactAgent_pk" PRIMARY KEY ("requestId")
+) WITH (
+  OIDS=FALSE
 );
 
-CREATE TABLE "House Data Summary"
-(
- "houseID"     integer NOT NULL,
- "price"       integer NOT NULL,
- "bedCount"    smallint NOT NULL,
- "bathCount"   smallint NOT NULL,
- "address"     text NOT NULL,
- "listingType" text NOT NULL,
- "zestimate"   integer NOT NULL,
- "estPayment"  integer NOT NULL,
- CONSTRAINT "PK_House Data Summary" PRIMARY KEY ( "houseID" )
+CREATE TABLE "ListingAgents" (
+	"houseId" integer NOT NULL,
+	"agentId" integer NOT NULL
+) WITH (
+  OIDS=FALSE
 );
 
 
+ALTER TABLE "HouseData" ADD CONSTRAINT "HouseData_fk0" FOREIGN KEY ("primaryAgent") REFERENCES "Agents"("agentId");
 
+ALTER TABLE "ContactAgent" ADD CONSTRAINT "ContactAgent_fk0" FOREIGN KEY ("agentId") REFERENCES "Agents"("agentId");
+ALTER TABLE "ContactAgent" ADD CONSTRAINT "ContactAgent_fk1" FOREIGN KEY ("houseId") REFERENCES "HouseData"("houseId");
 
-
-
-
-
-
-
-
-
-
+ALTER TABLE "ListingAgents" ADD CONSTRAINT "ListingAgents_fk0" FOREIGN KEY ("houseId") REFERENCES "HouseData"("houseId");
+ALTER TABLE "ListingAgents" ADD CONSTRAINT "ListingAgents_fk1" FOREIGN KEY ("agentId") REFERENCES "Agents"("agentId");
 
